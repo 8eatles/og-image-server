31048c997d1d119d76147e5dbbda722ee0833cf8
8eatles <8eatles@naver.com>
2020-02-19 20:42:38 +0900

    코드 정리 및 css 적용

--- routes/og.js
+++ routes/og.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/:id', async function(req, res, next) {
   const found = await ImageMeta.findByPk(req.params.id);
   const { title, description, path } = found;
-  const imageSrc = `http://127.0.0.1:3000/static/${path.replace(/uploads/g, '').replace(/\\/g, '/')}`;
+  const imageSrc = `http://127.0.0.1:3000/static${path.replace(/uploads/g, '').replace(/\\/g, '/')}`;
   res.render('og', { title, description, imageSrc });
 });
 
